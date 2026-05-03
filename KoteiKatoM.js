8001a967891789c63061a9c22eb25f5d4e69c8f6
jacekossowski <oscarsrr@yahoo.com>
2026-05-03 17:13:49 -0400

    Adjustments

--- KoteiKatoM.js
+++ KoteiKatoM.js
@@ -207,7 +207,7 @@ class LogicalCube {
 size = 4;
 const cube = new LogicalCube(size);
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(44, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 const container = document.getElementById('threejs-container');
